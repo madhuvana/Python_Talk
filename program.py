bc79703cba191a894365304359119e4321410257
Venktesha Bhat <venkteshab@DDC-MIN-VNKTSHB.local>
2016-12-02 18:32:49 +0530

    first App

---
+++ program.py
@@ -0,0 +1,12 @@
+print(' ************************************* ')
+print('')
+print('              Hello APP                ')
+print('')
+print(' ************************************* ')
+print('')
+
+user_text = input('enter your name?  ')
+
+greetings = user_text + '  Nice Meeting you  '
+print('')
+print(greetings)
